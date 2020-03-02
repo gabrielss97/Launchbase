efae801f8bbe8c7452c1cedbfee25d36524d098d
Gabriel Soares Salvador <gabriel.ssalvador97@gmail.com>
2020-03-01 22:23:27 -0300

    Primeiro HTML

--- Estudo HTML-CSS/src/scripts.js
+++ Estudo HTML-CSS/src/scripts.js
@@ -2,12 +2,16 @@ const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card");
 const closeModal = document.querySelector(".close-modal");
 
-for(let card of cards) {
-    card.addEventListener("click", function(){
-        modalOverlay.classList.add("active")
-    });
+for (let card of cards) {
+  card.addEventListener("click", function() {
+    const videoId = card.getAttribute("id");
+    modalOverlay.classList.add("active");
+    modalOverlay.querySelector("iframe").src =
+      `https://www.youtube.com/embed/${videoId}`;
+  });
 }
 
-closeModal.addEventListener("click", function(){
-    modalOverlay.classList.remove("active");
-})
+closeModal.addEventListener("click", function() {
+  modalOverlay.classList.remove("active");
+  modalOverlay.querySelector("iframe").src = "";
+});
